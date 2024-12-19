bce2484c6bcee4c32b6e88c8dcc81a18b5d5b0b7
OmerKahveci03 <ok21b@fsu.edu>
2024-12-18 19:43:44 -0500

    capture functionality

--- chess.py
+++ chess.py
@@ -1,7 +1,44 @@
 # chess.py
 # This module handles the logic of the chess game. We recieve the moves from game.py as (row, col)
 
+# BASE GAME TO DO:
+# 1) Check Detection
+# 2) Castling
+# 3) En Peassant
+# 4) Promotion
+
 from common import ROWS, COLS
+
+# global data
+turn_color = 'white'
+selected = None
+white_pieces = []
+black_pieces = []
+
+# returns the piece located at (row, col)
+def piece_at(row, col):
+    for piece in white_pieces + black_pieces:
+        if piece.row == row and piece.col == col:
+            return piece
+    return None
+
+# returns true if the given (row, col) location has an enemy piece
+def enemy_piece_at(row, col):
+    the_piece = piece_at(row, col)
+
+    if the_piece is not None:
+        if the_piece.color != turn_color:
+            return True
+    return False
+
+# removes the piece at (row, col) from the game, simulating a capture
+def capture_piece_at(row, col):
+    for piece in white_pieces:
+        if piece.row == row and piece.col == col:
+            white_pieces.remove(piece)
+    for piece in black_pieces:
+        if piece.row == row and piece.col == col:
+            black_pieces.remove(piece)
 
 class Piece():
     def __init__(self, row, col, type, color):
@@ -9,17 +46,8 @@ class Piece():
         self.col = col
         self.type = type
         self.color = color
-
-# returns true if the given (row, col) location has an enemy piece
-    def enemy_piece_at(self, row, col):
-        the_piece = piece_at(row, col)
-
-        if the_piece is not None:
-            if the_piece.color != self.color:
-                return True
-        return False
     
-# returns true if the given (row, col) move is legal for this piece
+    # returns true if the given (row, col) move is legal for this piece
     def is_valid_move(self, row, col):
         is_legal = True
         the_piece = piece_at(row, col)
@@ -31,9 +59,10 @@ class Piece():
         if 0 > row or row > ROWS or 0 > col or col > COLS:
             is_legal = False
 
-        # if king is left in check, is_legal = False
+        # (need) if king is left in check, is_legal = False
         return is_legal
 
+    # reusable code for pieces that move in straight or diagonal lines
     def directional_moves(self, directions):
         move_list = []
         for direction in directions:
@@ -43,18 +72,23 @@ class Piece():
                 while self.is_valid_move(row, col):
                     move_list.append((row, col))
                     i += 1
-                    if self.enemy_piece_at(row, col):
+                    if enemy_piece_at(row, col):
                         break
                     row = self.row + direction[0] * i
                     col = self.col + direction[1] * i
         return move_list
 
+    # moves the piece from its initial position to (row, col)
     def move(self, row, col):
-        # remove enemy piece if it is here
-        if self.enemy_piece_at(row, col):
-             pass
+        global turn_color
+        capture_piece_at(row, col)
         self.row = row
         self.col = col
+        if turn_color == 'white':
+            turn_color = 'black'
+        else:
+            turn_color = 'white'
+        print(f"{turn_color}'s turn.")
 
     # returns a list of coordinates (row, col) that this piece is allowed to move to
     def valid_moves(self):
@@ -67,13 +101,13 @@ class Piece():
                 direction = -1
                 start = 6
 
-            if not self.enemy_piece_at(self.row + direction, self.col): # basic move
+            if not enemy_piece_at(self.row + direction, self.col): # basic move
                 move_list.append((self.row + direction, self.col))
-                if self.row == start and not self.enemy_piece_at(self.row + direction * 2, self.col):
+                if self.row == start and not enemy_piece_at(self.row + direction * 2, self.col):
                     move_list.append((self.row + direction * 2, self.col))
-            if self.enemy_piece_at(self.row + direction, self.col + 1): # capture moves
+            if enemy_piece_at(self.row + direction, self.col + 1): # capture moves
                 move_list.append((self.row + direction, self.col + 1))
-            if self.enemy_piece_at(self.row + direction, self.col - 1): # capture moves
+            if enemy_piece_at(self.row + direction, self.col - 1): # capture moves
                 move_list.append((self.row + direction, self.col - 1))
 
             return move_list    # pawns follow unique rules, so I'm returning it before the filter
@@ -122,21 +156,8 @@ def initialize_army(army, color):
     for i in range(0,8):
         army.append(Piece(rows[1], i, 'pawn', color))
 
-# global data
-turn_color = 'white'
-selected = None
-
-white_pieces = []
-black_pieces = []
 initialize_army(white_pieces, 'white')
 initialize_army(black_pieces, 'black')
-
-# returns the piece located at row, col
-def piece_at(row, col):
-    for piece in white_pieces + black_pieces:
-        if piece.row == row and piece.col == col:
-            return piece
-    return None
 
 # if selected is None, select the piece. Otherwise, try moving the piece to the selected spot, and set selected to None
 def board_clicked(row, col,):
@@ -158,12 +179,7 @@ def board_clicked(row, col,):
             if temp is piece:
                 # try to move, or reject
                 if (row, col) in piece.valid_moves():
-                    piece.move(row, col, white_pieces, black_pieces)
-                    if turn_color == 'white':
-                        turn_color = 'black'
-                    else:
-                        turn_color = 'white'
-                    print(f"{turn_color}'s turn.")
+                    piece.move(row, col)
                 else:
                     print("Invalid Move!")
                 selected = None
